756c9196410b7f6060a17d59423b9ab5c82127b6
julierealmids <jullietnakayizaa@gmail.com>
2022-08-21 21:34:03 +0300

    Javascript

---
+++ style.js
@@ -0,0 +1,23 @@
+// import fetch from 'node-fetch'
+let fetch = require('node-fetch')
+
+fetch('https://jsonplaceholder.typicode.com/todos',{
+    method:'POST',
+    headers:{
+        "Content-Type": "application/json"
+    },
+    body:JSON.stringify({
+        userId:5,
+        title:"Hopper Lab",
+        completed:true
+    })
+})
+.then(function (response) {
+return response.json();
+})
+.then(function (data) {
+console.log('success', data);
+})
+.catch(function (error) {
+console.log('error', error);
+});
